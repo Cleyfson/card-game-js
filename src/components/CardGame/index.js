ee600fff8b09e4d7f4cf9b6cf6821cede61782fd
Cleifson Araujo <cl.raujo1@gmail.com>
2022-09-20 05:34:46 -0300

    :tada: Create CardGame Component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+const CardGame = () => {
+  return `
+    <article class='card-game'>
+      <img src='./src/images/alura-pixel1.png'>
+    </article>
+  `;
+};
+
+export default CardGame;
