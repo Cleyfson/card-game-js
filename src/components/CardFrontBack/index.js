72c715c2ce08692fa57a6538b9b9f099ded2c52c
Cleifson Araujo <cl.raujo1@gmail.com>
2022-09-26 13:04:11 -0300

    :sparkles: add the new icons to CardFrontBack

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -3,8 +3,10 @@ import CardGame from '../CardGame';
 const CardFrontBack = () => {
   return /*html*/ `
     <article class='card-front-back'>
-      ${CardGame()}
-      ${CardGame()}
+      ${CardGame('javascript', 'logo do javascript')}
+      ${CardGame('css', 'logo do css')}
+      ${CardGame('html', 'logo do html')}
+      ${CardGame('alura', 'logo da alura')}
     </article>
   `;
 };
