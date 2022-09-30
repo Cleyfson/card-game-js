7e5c5ca90117b91e417aed47d1a76547993c0707
Cleifson Araujo <cl.raujo1@gmail.com>
2022-09-30 15:09:21 -0300

    :bug: Swap player after cards are clicked

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -18,16 +18,40 @@ const BoardGame = () => {
     },
   ];
 
+  const hideCards = ($cardsActive) => {
+    $cardsActive.forEach((card) => card.classList.remove('-active'));
+  };
+
+  const changePlayer = () => {
+    const $arrowDown = document.querySelector('.arrow-down');
+    const value = $arrowDown.getAttribute('data-currentPlayer');
+    $arrowDown.setAttribute('data-currentPlayer', value == 1 ? 2 : 1);
+  };
+
+  window.boardGame = {};
+  window.boardGame.handleClick = () => {
+    const $boardGame = document.querySelector('.board-game');
+    const $cardsActive = $boardGame.querySelectorAll(
+      '.card-front-back.-active'
+    );
+
+    if ($cardsActive.length === 2) {
+      setTimeout(() => {
+        hideCards($cardsActive);
+        changePlayer();
+      }, 1000);
+    }
+  };
+
   const $htmlCardsList = cards.map((card) => {
     return (
       CardFrontBack(card.icon, card.alt) + CardFrontBack(card.icon, card.alt)
     );
   });
-
   const $htmlCards = $htmlCardsList.join('');
 
   return /*html*/ `
-    <section class='board-game'>
+    <section class='board-game' onClick='boardGame.handleClick()' >
       ${$htmlCards}
     </section>
   `;
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -7,7 +7,7 @@ import './style.css';
 const ScoreBoard = () => {
   return /*html*/ `
     <header class='score-board'>
-      ${ArrowDown(2)}
+      ${ArrowDown()}
       ${PlayerName('Player1')}
       ${PlayerScore(1)}
       ${VsPlayer()}
